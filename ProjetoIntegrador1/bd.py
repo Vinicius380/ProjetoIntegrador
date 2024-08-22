bc5a710bb4a2718392d579780f9402845f6b46f7
ViniciusJS <163647405+Vinicius380@users.noreply.github.com>
2024-08-21 22:13:24 -0300

    Projeto

---
+++ ProjetoIntegrador1/bd.py
@@ -0,0 +1,44 @@
+#SIMULACAO DO NOSSO BANCO DE DADOS
+
+Carros =  [
+    
+    #OBJETO 1
+    {
+        'id':1,
+        'marca': 'Toyota',
+        'modelo': 'Corolla',
+        'ano': 2009
+    },
+
+    #OBJETO 2
+    {
+        'id':2,
+        'marca': 'Toyota',
+        'modelo': 'RAV4',
+        'ano': 2015
+    },
+
+    #OBJETO 3
+    {
+        'id':3,
+        'marca': 'Fiat',
+        'modelo': 'Palio',
+        'ano': 2007
+    },
+
+    #OBJETO 4
+    {
+        'id':4,
+        'marca': 'Ford',
+        'modelo': 'Maverick',
+        'ano': 1960
+    },
+   
+    #OBJETO 5
+    {
+        'id':5,
+        'marca': 'Chevrolet',
+        'modelo': 'Chevette',
+        'ano': 1980
+    }
+]
